98043967b34c9227bd151c640c3c006930bb3382
Talha Turkyolu <turkyolutalha@gmail.com>
2021-02-17 23:22:09 -0600

    JD Ticketing Project Rest

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -3,8 +3,7 @@ VALUES ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, 'Admin'),
        ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, 'Manager'),
        ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, 'Employee');
 insert into users(insert_date_time, insert_user_id, is_deleted, last_update_date_time, last_update_user_id, enabled,
-                  first_name, gender, last_name, user_name, role_id,pass_word)
+                  first_name, gender, last_name, user_name, role_id, pass_word)
 values ('2021-01-05 00:00:00', 1, false, '2021-01-05 00:00:00', 1, true, 'admin', 'MALE', 'admin', 'admin@admin.com',
-        1,'$2a$10$nAB5j9G1c3JHgg7qzhiIXO7cqqr5oJ3LXRNQJKssDUwHXzDGUztNK');
+        1, '$2a$10$Q7ilQ6Hv11qpU0T7xfMzMeqxoPXkvhTVXxFqg0UL2xvLnhNqB7vba');
 
--- Abc1
